5e73dc6866ff725ffb611a09ba66df13ba0841aa
unknown <niteshkadian0668@gmail.com>
2026-02-05 11:28:46 +0530

    hahahehe

--- uncwi_sem_2/webdev/script.js
+++ uncwi_sem_2/webdev/script.js
@@ -241,9 +241,62 @@
 // }
 
 
-function goToPlayground(){
-    const p=new Promise((res,rej)=>{
-        let done=true;
-        setTimeout()
+
+//--------Multiple promise or chaining promise --------
+    function doHomework() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        let homeworkDone = true;
+        if (homeworkDone) {
+            console.log("Homework is done");
+            resolve("Homework complete");
+        } else {
+            reject("Homework not done");
+        }
+        }, 3000);
+    });
+    }
+    function eatDinner() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        let dinnerEaten = false;
+        if (dinnerEaten) {
+            console.log("Dinner is eaten");
+            resolve("Dinner complete");
+        } else {
+            reject(" Didn't eat dinner");
+        }
+        }, 2000);
+    });
+    }
+    function goToPlayground() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+        let permission = true;
+        if (permission) {
+            console.log(" Went to the playground");
+            resolve("Playground time!");
+        } else {
+            reject(" Not allowed to go out");
+        }
+        }, 3000);
+    });
+    }
+    //chaining
+
+    // Promise Chaining Starts
+    doHomework()
+    .then((data) => {
+    console.log(data);
+    return eatDinner();
     })
-}
+    .then((data) => {
+    console.log(data);
+    return goToPlayground();
+    })
+    .catch((error) => {
+    console.log("Something went wrong:", error);
+    })
+    .finally(() => {
+    console.log("Day ended — Go to sleep");
+    });
